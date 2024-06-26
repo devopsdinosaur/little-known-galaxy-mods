3315ee34847c78df4692c75976cc22a874c242f7
devopsdinosaur <devopsdinosaur@gmail.com>
2024-06-25 23:12:13 -0400

    Adding custom texture code... maybe

--- custom_textures/CustomTexturesPlugin.cs
+++ custom_textures/CustomTexturesPlugin.cs
@@ -3,75 +3,31 @@ using BepInEx.Logging;
 using BepInEx.Configuration;
 using HarmonyLib;
 using System;
-using System.Reflection;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using System.Reflection;
 
-[BepInPlugin("devopsdinosaur.lkg.testing", "Testing", "0.0.1")]
-public class TestingPlugin : BaseUnityPlugin {
+[BepInPlugin("devopsdinosaur.lkg.custom_textures", "Custom Textures", "0.0.1")]
+public class CustomTexturesPlugin : BaseUnityPlugin {
 
-	private Harmony m_harmony = new Harmony("devopsdinosaur.lkg.testing");
+	private Harmony m_harmony = new Harmony("devopsdinosaur.lkg.custom_textures");
 	public static ManualLogSource logger;
 	private static ConfigEntry<bool> m_enabled;
+	private static ConfigEntry<string> m_subdir;
 	
 	private void Awake() {
 		logger = this.Logger;
 		try {
 			m_enabled = this.Config.Bind<bool>("General", "Enabled", true, "Set to false to disable this mod.");
-			if (m_enabled.Value) {
-				this.m_harmony.PatchAll();
-			}
-
-            SceneManager.sceneLoaded += on_scene_loaded;
-
-			logger.LogInfo("devopsdinosaur.lkg.testing v0.0.1" + (m_enabled.Value ? "" : " [inactive; disabled in config]") + " loaded.");
+			m_subdir = this.Config.Bind<string>("General", "Texture Subfolder", "custom_textures", "Subfolder under this plugin's parent folder (i.e. <game>/BepInEx/plugins) which in turn contains a set of mod-specific texture directories.");
+			TextureManager.Instance.get_texture_paths(m_subdir.Value);
+			this.m_harmony.PatchAll();
+			logger.LogInfo("devopsdinosaur.lkg.custom_textures v0.0.1 loaded.");
 		} catch (Exception e) {
 			logger.LogError("** Awake FATAL - " + e);
 		}
 	}
-
-    public static bool list_descendants(Transform parent, Func<Transform, bool> callback, int indent) {
-		Transform child;
-		string indent_string = "";
-		for (int counter = 0; counter < indent; counter++) {
-			indent_string += " => ";
-		}
-		for (int index = 0; index < parent.childCount; index++) {
-			child = parent.GetChild(index);
-			logger.LogInfo(indent_string + child.gameObject.name);
-			if (callback != null) {
-				if (callback(child) == false) {
-					return false;
-				}
-			}
-			list_descendants(child, callback, indent + 1);
-		}
-		return true;
-	}
-
-	public static bool enum_descendants(Transform parent, Func<Transform, bool> callback) {
-		Transform child;
-		for (int index = 0; index < parent.childCount; index++) {
-			child = parent.GetChild(index);
-			if (callback != null) {
-				if (callback(child) == false) {
-					return false;
-				}
-			}
-			enum_descendants(child, callback);
-		}
-		return true;
-	}
-
-	public static void list_component_types(Transform obj) {
-		foreach (Component component in obj.GetComponents<Component>()) {
-			logger.LogInfo(component.GetType().ToString());
-		}
-	}
-
 
 	class CustomTexture {
 		private string m_name;
@@ -85,18 +41,31 @@ public class TestingPlugin : BaseUnityPlugin {
 		}
 
 		public Texture2D get_texture() {
-
+			return null;
 		}
 	}
 
 	class TextureManager {
-		
-		
+		private static TextureManager m_instance = null;
+		public static TextureManager Instance {
+			get {
+				if (m_instance == null) {
+					m_instance = new TextureManager();
+				}
+				return m_instance;
+			}
+		}
 		private Dictionary<string, CustomTexture> m_textures = new Dictionary<string, CustomTexture>();
 
+		public void get_texture_paths(string root_dir) {
+			try {
 
+			} catch (Exception e) {
+
+			}
+		}
 	}
-	
+
 	private static Dictionary<string, Sprite> m_replaced_sprites = new Dictionary<string, Sprite>();
 
 
@@ -161,33 +130,4 @@ public class TestingPlugin : BaseUnityPlugin {
 		}
 		return sprite;
 	}
-
-	/*
-	[HarmonyPatch(typeof(), "")]
-	class HarmonyPatch_ {
-
-		private static bool Prefix() {
-			try {
-
-				return false;
-			} catch (Exception e) {
-				logger.LogError("** XXXXX.Prefix ERROR - " + e);
-			}
-			return true;
-		}
-	}
-
-	[HarmonyPatch(typeof(), "")]
-	class HarmonyPatch_ {
-
-		private static void Postfix() {
-			try {
-
-				
-			} catch (Exception e) {
-				logger.LogError("** XXXXX.Postfix ERROR - " + e);
-			}
-		}
-	}
-	*/
 }
